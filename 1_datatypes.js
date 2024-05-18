4c2e84e80d55af92c1c922b549faf6a6fba0b895
Mahesh Mittha <maheshmittha93@gmail.com>
2024-05-18 18:25:51 +0530

    parseint parsefloat

--- 1_datatypes.js
+++ 1_datatypes.js
@@ -1,9 +1,34 @@
+// Data Type Nuber  
+
 var myFavNun = 5;
-console.log(myFavNun);
+// console.log(myFavNun);
 
+
+// Data Type String
 var myName = "Mahesh";
-console.log(myName);
-
+// console.log(myName);
 
 var mylastName = "Mittha"
-console.log(mylastName);
+// console.log(mylastName);
+
+// Data Type Boolean
+
+var isRaining = false;
+var areYouAwesome = true;
+// console.log(isRaining, areYouAwesome);
+
+// dataType Undefined
+
+var mahesh;
+// console.log(mahesh);
+
+// 
+
+var data = null;
+// console.log(data);
+
+
+// what is purpsose of type of operator
+
+let names = "mahesh";
+console.log(typeof names);
