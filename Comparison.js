54e8977a79c80b580c4f01e6001dc5c3ee4a576e
Mahesh Mittha <maheshmittha93@gmail.com>
2024-05-22 10:50:20 +0530

    comparison

--- Comparison.js
+++ Comparison.js
@@ -1,12 +1,9 @@
-
-
 // 1 Equality(==) Comparison Operator in JavaScript
 
-// The comparison operator returns true only if the value of two operands are equal otherwise it returns false 
+// The comparison operator returns true only if the value of two operands are equal otherwise it returns false
 
-// console.log(5 == 5); 
+// console.log(5 == 5);
 // console.log("5" == 5);
-
 
 //2 Strict Equality(===) Comparison Operator in JavaScript
 // JavaScript Strict Equality Operator is used to compare two operands and return true if both the value and type of operands are the same.
@@ -16,21 +13,43 @@
 
 // 3 Inequality(!=) Comparison Operator in JavaScript
 
-
 //3 JavaScript Inequality operator is used to compare two operands and returns true if both the operands are unequal
 
 //console.log(10 != 10);  // Bothe operanods should be unequal return true otherwise false
-// console.log( 25 != 15); 
+// console.log( 25 != 15);
 
-// 4 Checks if the left operator is greater than or equal to the right operator
+// 4 Less Than(<) Comparison Operator in JavaScript
 
-console.log(5>2);
+// JavaScript Less Than(<) Operator is used to compare two operands and return true if the left operand has a lesser value than the right operator
 
+// console.log( 10 < 15);  // True
+// console.log( 15 < 10);  // false
 
+// 5 Greater Than or Equal(>=) Comparison Operator in JavaScript
 
+// JavaScript Greater Than or Equal Operator(>=) is used to compare two operands and return true if the left operand has a value greater than or equal to the right operand.
 
+let m = 10;
+let n = 15;
+//  console.log(m >= n); // false
+//  console.log( 100 >= 200); //false
+//  console.log(1000 >= 1000); // true
+//  console.log(10 <= 10 ); // true
+//console.log(500 >= 700); //true
+// console.log(1000 > 1200); // false
+// console.log(1000 >= 1200); //fasle
+// console.log(2024 >= 2000 ); // true
+// console.log(2305 >= 10000);  // false// 
 
+// 6 Less Than or Equal(<=) Comparison Operator in JavaScript
 
+// JavaScript Less Than or Equal(<=) to the operator is used to compare two operands and return true if the left operand is smaller or equal to the right operand.
 
-
+//  console.log( 10 <= 15); // true
+//  console.log(100 <= 500); // true
+//  console.log(300 <= 457); //true
+//  console.log(1000 <= 1200); // true
+// console.log(873 <= 500); // false
+// console.log(7946 <= 800); // fasle 
+// console.log(2024 <= 1993); //  false
 
