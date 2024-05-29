a0941dc6d22cf765b3ee7fa5f6cf0a741ad6e2e8
Mahesh Mittha <maheshmittha93@gmail.com>
2024-05-29 09:05:45 +0530

    nested statement

--- Loops/IfElse.js
+++ Loops/IfElse.js
@@ -1,31 +1,66 @@
 "use strict";
 
+// let temp = 40;
 
-let temp = 40;
-
-if (temp < 30) {
-    console.log("I would like to go Goa");
-} else {
-    console.log("i would like to do javascript parctice at home");
-}
-
+// if (temp < 30) {
+//     console.log("I would like to go Goa");
+// } else {
+//     console.log("i would like to do javascript parctice at home");
+// }
 
 // 1.  Question  //
 
-let time = 12;  // PM
+// let time = 12;  // PM
 
-if (time > 13 ) {
-    console.log("Good Afternoon");
-} else if (time <= 12 && time > 16  ) {
-    console.log("Good Evening");
-} else {
-    console.log("Good Morning");
-}
-
+// if (time > 13 ) {
+//     console.log("Good Afternoon");
+// } else if (time <= 12 && time > 16  ) {
+//     console.log("Good Evening");
+// } else {
+//     console.log("Good Morning");
+// }
 
 // 2.  Question //
 
+// let age = 30;
+// let citizen = true
+// let registeredVoter = true;
+// if (age >= 18 && citizen == true && registeredVoter == true)   {
+//     console.log("Eligible to vote");
+// } else if (age > 18 != citizen && !registeredVoter) {
+//  console.log("Not Eligible to vote");
+// } else (age <=18 && !citizen )
+// console.log("Due to citizenship status");
+
+// Requitement  3:14:24
+
+let user = 19;
+let isCitizen = true;
+let isVoterregistered = false;
+
+if ((user) => 18) {
+  if (isCitizen) {
+    if (isVoterregistered) {
+      console.log("Your are eligibe to vote");
+    } else {
+      console.log("Your are not a member of registered voter group ");
+    }
+  } else {
+    console.log("You are not citenzeship of our country");
+  }
+} else {
+  console.log("Your are not eligible to vote (Younger)");
+}
 
 
+// Write programm s given Number is even or odd 
+
+let num = 11;
+
+if (num % 2  == 0) {
+    console.log("num is even");
+} else {
+    console.log("num is odd");
+}
 
 
