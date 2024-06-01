889a628e15e22a29c9e70e769f1a75c33276b641
Mahesh Mittha <maheshmittha93@gmail.com>
2024-06-01 12:59:53 +0530

    parctice

--- Loops/Switch.js
+++ Loops/Switch.js
@@ -26,59 +26,116 @@
 
 // simple mathematical calculations
 
-let opreation = 50;
-let num1 = 100;
-let num2 = 50;
-let result;
+// let opreation = 50;
+// let num1 = 100;
+// let num2 = 50;
+// let result;
 
-switch (opreation) {
-  case (result = num1 + num2):
-    console.log(result + " " + " Addition");
-    break;
+// switch (opreation) {
+//   case (result = num1 + num2):
+//     console.log(result + " " + " Addition");
+//     break;
 
-  case (result = num1 - num2):
-    console.log(result + " " + "Subtract");
-    break;
+//   case (result = num1 - num2):
+//     console.log(result + " " + "Subtract");
+//     break;
 
-  case (result = num1 * num2):
-    console.log(result + " " + " Multiply");
-    break;
+//   case (result = num1 * num2):
+//     console.log(result + " " + " Multiply");
+//     break;
 
-  case (result = num1 % num2):
-    console.log(result + " " + " Module");
-    break;
+//   case (result = num1 % num2):
+//     console.log(result + " " + " Module");
+//     break;
 
-  default:
-    console.log("No result");
-    break;
-}
+//   default:
+//     console.log("No result");
+//     break;
+// }
 
 // Area of crircle calculation
 
-let areaofShape = "circle";
-let a = 10;
-let b = 20;
-let count;
+// let areaofShape = "circle";
+// let a = 5;
+// let b = 10;
+// let count;
 
-switch (areaofShape) {
-  case "square":
-   (count = a * b)
-    console.log(count);
-    break;
+// switch (areaofShape) {
+//   case "square":
+//    (count = a * a)
+//     console.log(count);
+//     break;
 
-  case "reactangle":
-   (count = b * a)
-    console.log(count);
-    break;
+//   case "reactangle":
+//    (count = a * b)
+//     console.log(count);
+//     break;
     
- case "circle" :
- var radius = 2;
- result = 3.142 * (radius * radius)  
- console.log(result);   
- break;
+//  case "circle" :
+//  var radius = 2;
+//  result = 3.142 * (radius * radius)  
+//  console.log(result);   
+//  break;
 
-  default: console.log("No Shape Found");
+//   default: console.log("No Shape Found");
+//     break;
+// }
+
+
+
+
+// switch (key) {
+//   case value:
+    
+//     break;
+
+//   default:
+//     break;
+// }
+
+
+let month = 15 ;
+
+switch (month) {
+  case 1:
+  console.log("January");  
+    break;
+  case 2:
+  console.log("February");  
+    break; 
+  case 3:
+  console.log("March");   
+    break; 
+  case 4:
+  console.log("April");   
+    break; 
+  case 5:
+  console.log("May");   
+    break; 
+  case 6:
+  console.log("June");  
+    break; 
+  case 7:
+  console.log("July");  
+    break; 
+  case 8:
+  console.log("August");   
+    break; 
+  case 9:
+  console.log("September");   
+    break; 
+  case 10:
+  console.log("October");   
+    break; 
+  case 11:
+  console.log("November");   
+    break; 
+  case 12:
+  console.log("December");   
+    break; 
+  default: console.log("Default case matches");
     break;
 }
+
 
 
---
+++ Loops/While.js
@@ -0,0 +1,195 @@
+"use strict";
+
+let count = 1;
+// while (count <= 10) {
+//     console.log(count);
+//     count++; // 2
+// }
+
+
+// let i = 1;
+// do {
+//     console.log("first print result");
+//     i++;
+// } while ( i <= 10 );
+
+// let i = 1;
+// for ( i =1; i<= 10; i++){
+//     console.log(i);
+// }
+
+
+// Write a table 
+
+
+// let number = 1;
+
+// while (number <=10) {
+//     console.log(`5 * ` + number + ` = ` + (5*number));
+//     number++;
+// }
+
+
+// let multiple = 1;
+// while (multiple <= 10) {
+//     console.log(`10 * ${multiple} = ${10 * multiple}`);
+//     multiple++;
+//  }
+
+//  let myNumber = 10;
+//  while (myNumber <= 120) {
+//     console.log(myNumber);
+//     myNumber++ ;
+//  }
+
+
+// let counter = 1;
+// do {
+//     console.log(counter);
+//     counter ++;
+// } while (counter <=5);
+
+
+
+// let userInput;
+// let positiveNumber;
+
+// do {
+//     userInput = prompt("Enter valid number");
+//     positiveNumber = parseInt(userInput);
+// } while (isNaN(positiveNumber) || positiveNumber <= 0);
+// console.log(userInput);
+
+
+// Write program sum of 1 to 10 
+
+// let sum = 0;
+// for ( let num = 1; num <= 5; num++) {
+//     debugger;
+//     let sum = sum + num;
+// }
+
+//console.log(sum);
+
+
+// let i = 5;
+// for ( let i = 5; i<51; i = i + 5) {
+//     console.log(i);
+// }
+
+
+
+// let index = 0;
+// while (index < 10 ) {
+//     console.log(`value of index ${index}`);
+//     index = index + 2;
+// }
+
+
+// let myArray = [1, 2, 3, 4];
+// let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(`value  ${arr}`);
+//     arr = arr + 1;
+// }
+
+
+// let isUserLogged = false;
+// if (isUserLogged) {
+//     console.log("Logged in sucessfully");
+// } else {
+//     console.log(" Please log in first");
+// }
+
+//console.log("Out side scope definetly logs");
+
+
+// let temprature = 52;
+
+// if (temprature < 50) {
+//     console.log("Temprature is less than 50");
+// } else {
+//     console.log("Temprature is greater than 50");
+// }
+
+
+// const score = 10;
+
+// if (score < 20) {
+//     let power = "fly";
+//     console.log(`Score: ${power}`);
+// }
+
+
+
+// let balance = 1000;
+// if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("greater than 1200");
+// }
+
+
+// let userName = "Mahesh";
+// let userEmail = "mahesh@gmail.com";
+// let userLoggedIn = true;
+// let userDebitCard = true;
+
+
+// if ( userName && userEmail && userLoggedIn && userDebitCard ) {
+//     console.log("user successfully logged in");
+// } else {
+//     console.log(" Please have user information");
+// }
+
+
+
+// let userLoggedInFromGoogle = true;
+// let userLoggedInFromEmail = true;
+// let guestUser = null;
+
+// if ( userLoggedInFromEmail ||  userLoggedInFromGoogle || guestUser ) {
+//     console.log("Logged in");
+// } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
