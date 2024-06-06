a2a89b0d9011b861f7bba628182a5a9b4df5f938
Mahesh Mittha <maheshmittha93@gmail.com>
2024-06-06 10:36:13 +0530

    ES6

---
+++ EcmaScript/ecma15.js
@@ -0,0 +1,91 @@
+// Let vs const
+
+// var name = "mahesh";
+
+// if(name) {
+// var name = "ganesh";
+// console.log(name);
+// }
+
+// name = "solapur";
+// console.log(name);
+
+// 1 Let varibales declaration
+
+// let myName = "rohit";
+
+// if (myName) {
+//    let myName = "Ms Dhoni";
+//    console.log(myName);
+// }
+
+// myName = "virat kohli";
+// console.log(myName);
+
+// 2. Const Variables declarations
+
+// const myCity = "pune";
+
+// if(myCity) {
+//     const myCity = "mumbai";
+// }
+
+// const myCity = "hyderbad";
+// console.log(myCity);
+
+// 3. Template Literals
+
+// let myCity = "Hyderbad";
+// let myState = "Telangana";
+// let myCountry = `${myCity} ${myState}`;
+// console.log(`${myCountry}`);
+
+// let age = 30;
+// console.log(`i am ${age} years old`);
+
+// let message = `Thus is multiline text 
+// with the following characters 
+// and characters in it
+// `;
+// console.log(message);
+
+
+// let num = 5;
+// for(let num = 1; num <=10; num++) {
+//     console.log(` 5 * ${num} =  ${5 * num} `);
+// }
+
+
+// 4. Default Parameters ***************** //
+
+// function sum(a , b) {
+//     return a + b;
+// }
+// console.log(sum(10 , 10));
+
+
+// 5 . Fat Arrow Function
+
+
+// const sumOne = (a , b ) => {
+//     let result = `The sum of ${a + b}`;
+//     console.log(result);
+// }
+// console.log(sumOne(2, 2));
+
+
+// let add = (a,b) => console.log(`Adding ${a + b} `);
+// add(2, 2);
+
+
+// let devide = (a,b) => console.log(`Devide ${a % b } even number ` );
+// devide(13, 2);
+
+
+
+
+
+
+
+   
+
--- Function/Function.js
+++ Function/Function.js
@@ -1,10 +1,8 @@
 //  ***** Function in JavaScript ***** ///
 
-
 //1.   What is Function Declaration
 
-
-// 1 Normal function declaration 
+// 1 Normal function declaration
 // Definition - It declares a function with a function keyword. The function declaration must have a function name.
 
 // function square(num) {
@@ -13,26 +11,24 @@
 
 // console.log(square(2,2)  );
 
-
-// 2.  Function Expression - 
-// Definition - It is similar to a function declaration without the function name. Function expressions can be stored in a variable assignment. 
+// 2.  Function Expression -
+// Definition - It is similar to a function declaration without the function name. Function expressions can be stored in a variable assignment.
 
 // const addition = function (num) {
 //     return num + num;
 // }
 // console.log((addition(2,2)));
 
-// 3.  Anonomysis  Function - 
+// 3.  Anonomysis  Function -
 
 // function () {
 
 // }
 
-
-// 4.   What is first calss function 
+// 4.   What is first calss function
 
 // function sqaure (num) {
-//     return num * num;   
+//     return num * num;
 // }
 
 // function dispalySquare(fn) {
@@ -41,9 +37,7 @@
 
 // dispalySquare(sqaure);
 
-
 //  write a program 3 student at same time to find of two number
-
 
 // a = 10;
 // b = 20;
@@ -57,7 +51,6 @@
 // b = 50;
 // let sumThree = a + b ; console.log(`Third Student sum of is ${sumThree}`);
 
-
 // const student = function name(a , b) {
 //     return a + b;
 // }
@@ -66,13 +59,11 @@
 // console.log((student(10,20)))
 // console.log((student(25,25)))
 
-
 // const greet = function (name , mesages) {
 //     console.log(`${name} , ${mesages}`);
-// } 
+// }
 
 // greet("Mahesh" , "Wolcome");
-
 
 // let i = 14;
 // let isPrime = false;
@@ -81,11 +72,11 @@
 
 //     for (let i = 0; i <= 12; i ++);
 //     if( i % 2 == 0 ) {
-//     let isPrime = false; 
+//     let isPrime = false;
 
-//     } 
+//     }
 
-//    if( isPrime ) { 
+//    if( isPrime ) {
 //     console.log("Prime Number");
 //    } else {
 //     console.log("Not Prime Number");
@@ -95,9 +86,7 @@
 
 // result(14);
 
-
-
-// function sum(a, b) { 
+// function sum(a, b) {
 //     console.log("before return keyword print , after return console does not print /excecute");
 //     return a + b;
 // }
@@ -105,24 +94,132 @@
 // let result = sum (10, 20);
 // console.log(result);
 
-
-
-
-// 1. First class function 
+// 1. First class function
 
 // in a language a function is trated like varibale there function are called are first class functions
 
-const square = function (num ) {
-    return num * num;
-}
+// const square = function (num ) {
+//     return num * num;
+// }
 
-function displaySqaure(fn) {
-    console.log( "Square is " + fn(5));
-}
+// function displaySqaure(fn) {
+//     console.log( "Square is " + fn(5));
+// }
 
-displaySqaure(square);
-
-
+// displaySqaure(square);
 
 // 2. IIFE immediately invoke function expression
+
+// (function (num){
+//     console.log(num + num);
+// }) ( 5, 10);
+
+// (function sqaure (x) {
+//   return (function (y) {
+//     console.log(x);
+//  })(2);
+// })(1);
+
+// (function (a) {
+//     (function (y) {
+//         console.log(a);
+//     })(2);
+// })(1)
+
+// CheckOut - Function Scope - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+
+// The following variables are defined in the global scope
+
+const num1 = 20;
+const num2 = 3;
+const myName = "Mahesh";
+
+// This function is defined in the global scope
+
+// function multiply () {
+//     return num1 * num2;
+// }
+
+// console.log(multiply());
+
+// function getScore () {
+//     const num1 = 2;
+//     const num2 = 3;
+
+//     function add () {
+//         return `${myName} Scored  ${num1 + num2}`;
+//     }
+
+//     return add();
+// }
+
+// console.log(getScore());
+
+// function sum (a,b) {
+//     console.log(a+b);
+// }
+
+// console.log(sum(5,5));
+
+// function multiply (a,b) {
+//     return a*b;
+// }
+// let result = multiply(5,5);
+// console.log(result);
+// console.log(`The multiple of two number is ${result}`);
+
+// (function(a,b) {
+//     console.log(a+b);
+// })(5, 10);
+
+// var result = (function (x,y,z) {
+//     var x= 1;
+//     var y = 2;
+//     var z = 3;
+//     return x+y+z;
+// })();
+
+// console.log(result);
+
+// var counter = function () {
+//   var count = 0;
+
+//   return {
+//     increment: function () {
+//       count++;
+//     },
+
+//     decrement: function () {
+//       count--;
+//     },
+
+//      getCount : function () {
+//         return count;
+//      }
+//   };
+
+// }();
+
+// counter.increment();
+// counter.increment();
+// counter.increment();
+
+// console.log(counter.getCount());
+// console.log(counter.decrement());
+
+
+const result = (function (a , b) {
+  console.log(a + b);
+return a + b;
+}) (2,3)
+console.log(result);
+
+
+
+
+
+
+
+
+
 
