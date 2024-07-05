72ce9ad69494f4f54f47b13851a4dd45ae9e794e
Mahesh Mittha <maheshmittha93@gmail.com>
2024-07-05 09:27:34 +0530

    firstchild

--- Window/Dom.js
+++ Window/Dom.js
@@ -15,12 +15,21 @@
 // console.log(parent);
 
 
+// let childBody = document.body;
+// let childHead = document.head;
+
+// let childNode =  document.body.childNodes;
+// let childChildren = document.body.children;
+// console.log(childNode);
+// console.log(childChildren);
+
+
 // ? *******  ChildNodes and Children ********** ///// 
 
-let bodyElement = document.body;
-let headElement = document.head;
-let bodyChild = document.body.childNodes;   // Return -  The childNodes property returns a NodeList .
-let bodyChildNode =  document.body.children // Return -  The children property returns a collection of an element's child elements.
+// let bodyElement = document.body;
+// let headElement = document.head;
+// let bodyChild = document.body.childNodes;   // Return -  The childNodes property returns a NodeList . (DOM Tree data )
+// let bodyChildNode =  document.body.children // Return -  The children property returns a collection of an element's child elements.
 //console.log(bodyChild); 
 //console.log(bodyChildNode); 
 
@@ -29,10 +38,63 @@ let bodyChildNode =  document.body.children // Return -  The children property r
 // ? ****** FirstChild and FirstElementChild ******* ///
 
 
-let firstChild = bodyElement.firstChild; //
-let firstElementChild = bodyElement.firstElementChild;
+// let firstChild = bodyElement.firstChild; 
+// let firstElementChild = bodyElement.firstElementChild;
+// console.log(firstChild);
+// console.log(firstElementChild);
+
+
+
+// let childNode =  document.body.childNodes;
+// console.log(childNode);
+
+let childChildren = document.body.children;
+console.log(childChildren);
+
+
+let firstChild = document.firstChild;
 console.log(firstChild);
-console.log(firstElementChild);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
